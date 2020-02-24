1d29946fed149c1e7f6a8d4932f1c8e7ec30bf3f
k4sperle <wendland.christian.86@gmail.com>
2020-02-24 16:42:36 +0100

    Screens angelegt

---
+++ screens/BwScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, Button, InputText, StyleSheet} from 'react-native';
+
+const BwScreen = props => {
+    return (
+        <View style={style.screen}>
+
+        </View>
+    )
+};
+
+const style = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignmentItems: 'center'
+    }
+});
+
+export default BwScreen;
