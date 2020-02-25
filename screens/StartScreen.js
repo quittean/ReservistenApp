4b441bc61a17a4f20f9b2b98fd6ef2a94ee20572
Petefish88 <41153517+Petefish88@users.noreply.github.com>
2020-02-25 11:56:10 +0100

    Update StartScreen.js

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -7,37 +7,42 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 export default class StartScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        
-        <Text style={{}}>Die Reservisten App</Text>
+      <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{alignItems: ''}}>
+          <Text>Die Reservisten App</Text>
+        </View>
 
+        <View style={{flex: 1, flexDirection: 'row'}}>
          <TouchableOpacity onPress= {() => this.props.navigation.navigate('Bw')}>
-            <Image style={styles.button} source={require('../assets/iconbw.png')} />
+            <Image style={styles.icon} source={require('../assets/iconbw.png')} />
             <Text>Die Reserve</Text>
           </TouchableOpacity>
           
           <TouchableOpacity onPress= {() => this.props.navigation.navigate('Boerse')}>
-            <Image style={styles.button} source={require('../assets/iconboerse.png')} />
+            <Image style={styles.icon} source={require('../assets/iconboerse.png')} />
             <Text>Stellenangebote</Text>
           </TouchableOpacity> 
-          
+          </View>
+
+          <View style={{flex: 1, flexDirection: 'row'}}>
           <TouchableOpacity onPress= {() => this.props.navigation.navigate('Cal')}>
-            <Image style={styles.button} source={require('../assets/iconcal.png')} />
+            <Image style={styles.icon} source={require('../assets/iconcal.png')} />
             <Text>Veranstaltungskalender</Text>
           </TouchableOpacity>
 
           <TouchableOpacity onPress= {() => { Linking.openURL('https://bundeswehr.community')}}>
-            <Image style={styles.button} source={require('../assets/iconcommunity.png')} />
+            <Image style={styles.icon} source={require('../assets/iconcommunity.png')} />
             <Text>Bundeswehr Community</Text>
           </TouchableOpacity>
+          </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  button: {
-    width: 50,
+  icon: {
+    width: 100,
     height: 50
   },
 });
