cf7b313b5f3e46e58f4e5873e4295bb86ae92e71
Petefish88 <41153517+Petefish88@users.noreply.github.com>
2020-02-25 15:53:04 +0100

    lol

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -8,11 +8,11 @@ export default class StartScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <View style={{alignItems: ''}}>
-          <Text style={{width: '100%', fontSize: '3rem'}}>Die Reservisten App</Text>
+        <View style={{alignItems: 'center'}}>
+          <Text style={{width: '100%', fontSize: 50}}>Die Reservisten App</Text>
         </View>
 
-        <View style={{flexDirection: 'row', borderWidth: 2, width: '80%', height: '40%', justifyContent: "space-evenly"}}>
+        <View style={{flexDirection: 'row', borderWidth: 2, width: '80%', height: '40%', justifyContent: 'space-evenly', alignItems: 'center'}}>
          <TouchableOpacity onPress= {() => this.props.navigation.navigate('Bw')}>
             <Image style={styles.icon} source={require('../assets/iconbw.png')} />
             <Text style={styles.buttonText}>Die Reserve</Text>
@@ -22,9 +22,9 @@ export default class StartScreen extends Component {
             <Image style={styles.icon} source={require('../assets/iconboerse.png')} />
             <Text style={styles.buttonText}>Stellenangebote</Text>
           </TouchableOpacity> 
-          </View>
+        </View>
 
-          <View style={{flexDirection: 'row', borderWidth: 2, width: '80%', height: '40%', justifyContent: "space-evenly"}}>
+        <View style={{flexDirection: 'row', borderWidth: 2, width: '80%', height: '40%', justifyContent: 'space-evenly', alignItems: 'center'}}>
           <TouchableOpacity onPress= {() => this.props.navigation.navigate('Cal')}>
             <Image style={styles.icon} source={require('../assets/iconcal.png')} />
             <Text style={styles.buttonText}>Veranstaltungen</Text>
@@ -34,7 +34,7 @@ export default class StartScreen extends Component {
             <Image style={styles.icon} source={require('../assets/iconcommunity.png')} />
             <Text style={styles.buttonText}>Bw Community</Text>
           </TouchableOpacity>
-          </View>
+        </View>
       </View>
     )
   }
@@ -42,11 +42,11 @@ export default class StartScreen extends Component {
 
 const styles = StyleSheet.create({
   icon: {
-    width: 100,
+    width: 50,
     height: 50,
     alignSelf: "center"
   },
   buttonText: {
-    fontSize: '2rem'
+    fontSize: 23
   },
 });
