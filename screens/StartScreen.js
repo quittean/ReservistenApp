d40435fb6b30b58de10281b52ab29971de02892a
Petefish88 <41153517+Petefish88@users.noreply.github.com>
2020-02-25 16:14:57 +0100

    olo

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Icon, Button, View, Text, StyleSheet, Image, Linking, Card} from 'react-native';
+import { Icon, Button, View, Text, StyleSheet, Image, Linking, Card, ImageBackground} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -7,12 +7,13 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 export default class StartScreen extends Component {
   render() {
     return (
+      <ImageBackground source={require('../assets/background.jpg')} style={{width: '100%', height: '100%'}}>
       <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
         <View style={{alignItems: 'center'}}>
-          <Text style={{width: '100%', fontSize: 50}}>Die Reservisten App</Text>
+          <Text style={{width: '100%', fontSize: 35, color: 'white'}}>Die Reservisten App</Text>
         </View>
 
-        <View style={{flexDirection: 'row', borderWidth: 2, width: '80%', height: '40%', justifyContent: 'space-evenly', alignItems: 'center'}}>
+        <View style={{flexDirection: 'row', width: '80%', height: '40%', justifyContent: 'space-evenly', alignItems: 'center'}}>
          <TouchableOpacity onPress= {() => this.props.navigation.navigate('Bw')}>
             <Image style={styles.icon} source={require('../assets/iconbw.png')} />
             <Text style={styles.buttonText}>Die Reserve</Text>
@@ -24,7 +25,7 @@ export default class StartScreen extends Component {
           </TouchableOpacity> 
         </View>
 
-        <View style={{flexDirection: 'row', borderWidth: 2, width: '80%', height: '40%', justifyContent: 'space-evenly', alignItems: 'center'}}>
+        <View style={{flexDirection: 'row', width: '80%', height: '40%', justifyContent: 'space-evenly', alignItems: 'center'}}>
           <TouchableOpacity onPress= {() => this.props.navigation.navigate('Cal')}>
             <Image style={styles.icon} source={require('../assets/iconcal.png')} />
             <Text style={styles.buttonText}>Veranstaltungen</Text>
@@ -36,17 +37,20 @@ export default class StartScreen extends Component {
           </TouchableOpacity>
         </View>
       </View>
+      </ImageBackground>
     )
   }
 }
 
 const styles = StyleSheet.create({
   icon: {
-    width: 50,
-    height: 50,
+    width: '90%',
+    height: '80%',
+    resizeMode: 'contain',
     alignSelf: "center"
   },
   buttonText: {
-    fontSize: 23
+    fontSize: 18,
+    color: 'white'
   },
 });
