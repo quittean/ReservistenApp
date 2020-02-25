dfbd246c416073baa601d9ffcd3e34457ec544c5
Petefish88 <41153517+Petefish88@users.noreply.github.com>
2020-02-25 11:34:57 +0100

    StartScreen angepasst

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Icon, Button, View, Text, StyleSheet, Image, Linking, Card} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class StartScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-          <Button
-          title="Die Reserve"
-          onPress={() => this.props.navigation.navigate('Bw')}
-          />
-          <Button
-          title="Stellenangebote"
-          onPress={() => this.props.navigation.navigate('Boerse')}
-          />
-          <Button
-          title="Veranstaltungskalender"
-          onPress={() => this.props.navigation.navigate('Cal')}
-          />
+        
+        <Text style={{}}>Die Reservisten App</Text>
+
+         <TouchableOpacity onPress= {() => this.props.navigation.navigate('Bw')}>
+            <Image style={styles.button} source={require('../assets/iconbw.png')} />
+            <Text>Die Reserve</Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity onPress= {() => this.props.navigation.navigate('Boerse')}>
+            <Image style={styles.button} source={require('../assets/iconboerse.png')} />
+            <Text>Stellenangebote</Text>
+          </TouchableOpacity> 
+          
+          <TouchableOpacity onPress= {() => this.props.navigation.navigate('Cal')}>
+            <Image style={styles.button} source={require('../assets/iconcal.png')} />
+            <Text>Veranstaltungskalender</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress= {() => { Linking.openURL('https://bundeswehr.community')}}>
+            <Image style={styles.button} source={require('../assets/iconcommunity.png')} />
+            <Text>Bundeswehr Community</Text>
+          </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 50,
+    height: 50
+  },
+});
