9773e819129a60792fb0bbfe129d56386c41024a
herrfeder <d.lassig@t-online.de>
2020-02-26 13:48:33 +0100

    Adding Flask App

---
+++ scrape_api/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding: utf-8
+from flask import Flask, Blueprint
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+
+
+bp = Blueprint('diasterresponse', __name__)
+
+app = Flask(__name__)
+
+@bp.route('/getarticles')
+def getarticles():
+    engine = create_engine('sqlite:///scrapeoutput/bwscrape.db')
+    df = pd.read_sql_table('bwde_articles', engine)
+
+    return df.to_json(orient="index")
+
+app = Flask(__name__)
+app.register_blueprint(bp, url_prefix='/resbw')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6050, debug=False)
