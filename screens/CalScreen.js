28f385c4288202ffea0f5f7dff2c432b753c756a
Petefish88 <41153517+Petefish88@users.noreply.github.com>
2020-02-26 13:00:35 +0100

    Update CalScreen.js

--- screens/CalScreen.js
+++ screens/CalScreen.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import { Button, View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { WebView} from 'react-native-webview';
 
 export default class CalScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Veranstaltungskalender</Text>
-      </View>
+      <WebView
+          source={{ uri: 'https://www.reservistenverband.de/veranstaltungen/?kategorie=&landesid=&search=&distance=' }}
+          ref={c => this._webview = c}
+          renderLoading={this.renderLoading}
+          startInLoadingState
+          javaScriptEnabled={true}
+          injectedJavaScript={`
+          function removeElementsByClass(className){
+            var elements = document.getElementsByClassName(className);
+            while(elements.length > 0){
+                elements[0].parentNode.removeChild(elements[0]);
+            }
+          }
+          removeElementsByClass("main-nav");
+          removeElementsByClass("sidebar-widget sidebar-widget--veranstaltungen");
+          removeElementsByClass("footer");
+          removeElementsByClass("sticky-nav tab-nav text-uppercase");
+          removeElementsByClass("slider slider--state slider--teaser slick-initialized slick-slider");
+          removeElementsByClass("widget widget--social");
+          `}
+      />
     )
   }
 }
